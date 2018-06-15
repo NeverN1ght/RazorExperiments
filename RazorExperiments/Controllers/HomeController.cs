45492aff3bed960be98aeb4b33df4c078f6fe1f5
Mykola Kobets <nicko9298@gmail.com>
2018-06-15 15:28:46 +0300

    Добавьте файлы проекта.

---
+++ RazorExperiments/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RazorExperiments.Models;
+
+namespace RazorExperiments.Controllers
+{
+    public class HomeController : Controller
+    {
+        Product myProduct = new Product
+        {
+            ProductId = 1,
+            Name = "Kayak",
+            Description = "A boat for one person",
+            Category = "Watersports",
+            Price = 275M
+        };
+
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+    }
+}
---
+++ RazorExperiments/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RazorExperiments.MvcApplication" Language="C#" %>
